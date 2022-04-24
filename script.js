a95fd1a3eacb2ed7a461455ddd39e7e69bf362e7
sangram ghadage <sangramgha007@gmail.com>
2022-04-24 12:02:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const result = document.querySelector(".result");
+const decrease = document.querySelector(".decrease");
+const reset = document.querySelector(".reset");
+const increase = document.querySelector(".increase");
+
+
+decrease.addEventListener("click", () =>{
+    result.innerHTML--;
+    color()
+})
+
+
+increase.addEventListener("click", () =>{
+    result.innerHTML++;
+    color()
+})
+
+
+reset.addEventListener("click", () =>{
+    result.innerHTML = 0;
+    color()
+})
+
+
+function color () {
+    if (result.innerHTML < 0){
+        result.style.color = "#f03e3e"
+    }
+    else if(result.innerHTML > 0){
+        result.style.color  = "#37b24d"
+    }
+    else{
+        result.style.color = "#ffffff"
+    }
+}
